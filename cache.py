4f37518dc27e9f7f00d38b650bc336ffb9fdc836
Deanna Baris <dbaris01@eecs.tufts.edu>
2018-04-19 14:40:52 -0400

    two cache types - lru & lfu

--- cache.py
+++ cache.py
@@ -25,8 +25,14 @@ class Cache:
         self.capacity = capacity
         self.size = 0
         self.rank = 0
+
+        # Two data structures: a queue to manage which key to remove &
+        #   the cache itself (hash map)
         self.keyQueue = queue.PriorityQueue(maxsize=capacity)
         self.cache = {}
+
+    def size(self):
+        return self.size
 
     def print(self):
         print("Cache Contents:")
@@ -44,6 +50,21 @@ class Cache:
 
             self.keyQueue = newq
 
+    def add(self, url, html):
+        raise NotImplementedError("Subclass responsibility")
+
+    def clear(self):
+        self.cache = {}
+        self.keyQueue = queue.PriorityQueue(maxsize=self.capacity)
+        self.size = 0
+        self.rank = 0
+
+    # Returns page if found in cache, or None
+    def get(self, url):
+        return self.cache.get(url, None)        
+
+
+class LRU_Cache(Cache):
     def add(self, url, html):
         inCache = self.cache.get(url, None)
 
@@ -76,21 +97,42 @@ class Cache:
             self.keyQueue = newq
             self.rank += 1
 
+class LFU_Cache(Cache):
+    def add(self, url, html):
+        inCache = self.cache.get(url, None)
 
-    def clear(self):
-        self.cache = {}
-        self.keyQueue = queue.PriorityQueue(maxsize=self.capacity)
-        self.size = 0
-        self.rank = 0
+        # Not in cache 
+        if inCache is None:
+            # if cache is full, remove something
+            if self.size == self.capacity:
+                oldKey = self.keyQueue.get()
+                self.cache.pop(oldKey.get_id())
+                self.size -= 1
 
-    # Returns page if found in cache, or None
-    def get(self, url):
-        return self.cache.get(url, None)        
+            # add url/html to cache
+            self.cache[url] = html
+            self.size += 1
 
+            # add url to end of queue
+            self.keyQueue.put(Key(0, url))
+
+        else:
+            # move url location on queue
+            newq = queue.PriorityQueue(maxsize=self.capacity)
+
+            for i in range(0, self.size):
+                k = self.keyQueue.get()
+                if k.get_id() == url and self.cache[url] == html:
+                    k.priority += 1
+                newq.put(k)
+            
+            self.keyQueue = newq
+ 
 
 def test():
-    test_cap = 100
-    c = Cache(test_cap)
+    test_cap = 5
+    print("************ LRU CACHE TEST ************")
+    c = LRU_Cache(test_cap)
 
     for i in range(0, test_cap):
         c.add(str(i), "<html>" + str(i) + "</html>")
@@ -113,11 +155,29 @@ def test():
         print("FAILED TEST")
         return
 
+    print("************ LFU CACHE TEST ************")
+    c2 = LFU_Cache(test_cap)
+    c2.print()
+
+
+    for i in range(0, test_cap):
+        for j in range (0, i+1):
+            # print("Adding %d" %(i,))
+            c2.add(str(i), "<html>" + str(i) + "</html>")
+
+    c2.print()
+
+    c2.add("5", "<html>5</html>");
+    c2.add("5", "<html>5</html>");
+    c2.add("5", "<html>5</html>");
+    c2.print()
+
     print("PASSED (...probably)")
 
 
-def main():
-    test()
+# def main():
+#     test()
 
 
-main()
+
+# main()
