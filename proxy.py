c3a17c2096117eb7536a68202685a43575e00602
rgalbiati <raina.galbiati@tufts.edu>
2018-04-25 00:03:00 -0400

    divided into some subfuncs, still limited by https

--- proxy.py
+++ proxy.py
@@ -2,6 +2,59 @@ import socket
 import threading
 import signal
 import sys
+import ssl
+import re
+import subprocess
+
+
+from cache import LFU_Cache
+
+resolv = {}
+
+class Request :
+    def __init__(self, data) :
+        global resolv
+
+        first_line = data.split('\n')[0]                   
+
+        self.key_word = first_line.split(' ') [0]                    
+        self.url = first_line.split(' ') [1]                    
+
+        http_pos = self.url.find("://")          
+        if (http_pos==-1):
+            temp = self.url
+        else:
+            temp = self.url[(http_pos+3):]       
+        
+        port_pos = temp.find(":")           
+
+        webserver_pos = temp.find("/")
+        if webserver_pos == -1:
+            webserver_pos = len(temp)
+
+        self.host = ""
+        self.port = -1
+        if (port_pos==-1 or webserver_pos < port_pos):      
+            self.port = 80
+            self.host = temp[:webserver_pos]
+        else:                                               
+            self.port = int((temp[(port_pos+1):])[:webserver_pos-port_pos-1])
+            self.host = temp[:port_pos]
+
+        self.addr = None
+        if (self.host in resolv.keys()):
+            addr = resolv[self.host]
+        else:
+            tmp = subprocess.check_output(["nslookup", self.host])
+            tmp = tmp.decode()
+            results = tmp.split("\n")
+            for result in results:
+                match = re.match("^address:\t([^ ]+).*$", result.strip(), re.I)
+                if (match):
+                    self.addr = match.group(1).split("#")[0]
+            resolv[self.host] = self.addr
+        
+
 
 class Proxy :
     def __init__(self, config) :
@@ -34,72 +87,62 @@ class Proxy :
         self.shutdown(0,0)
         print("leaving")
 
-    def start_thread(self, conn, client_addr):
-        """
-        *******************************************
-        *********** PROXY_THREAD FUNC *************
-          A thread to handle request from browser
-        *******************************************
-        """
+    def start_thread(self, client_conn, client_addr):
 
-        request = conn.recv(config['MAX_LEN'])        # get the request from browser
-        request = request.decode()
-        first_line = request.split('\n')[0]                   # parse the first line
-        print("first line: ", first_line)
-
-        url = first_line.split(' ')[1]                        # get url
-        print("url: ", url)
-
-
-        # find the webserver and port
-        http_pos = url.find("://")          # find pos of ://
-        if (http_pos==-1):
-            temp = url
-        else:
-            temp = url[(http_pos+3):]       # get the rest of url
-
-        port_pos = temp.find(":")           # find the port pos (if any)
-
-        # find end of web server
-        webserver_pos = temp.find("/")
-        if webserver_pos == -1:
-            webserver_pos = len(temp)
-
-        webserver = ""
-        port = -1
-        if (port_pos==-1 or webserver_pos < port_pos):      # default port
-            port = 80
-            webserver = temp[:webserver_pos]
-        else:                                               # specific port
-            port = int((temp[(port_pos+1):])[:webserver_pos-port_pos-1])
-            webserver = temp[:port_pos]
-
-        print("webserver: ", webserver)
-        print("port: ", port)
+        r = client_conn.recv(config['MAX_LEN'])        # get the request from browser
         
-        try:
-            # create a socket to connect to the web server
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.settimeout(config['TIMEOUT'])
-            s.connect((webserver, port))
-            request = request.encode()
-            s.sendall(request)                           # send request to webserver
+        request = Request(r.decode())
 
-            while 1:
-                data = s.recv(config['MAX_LEN'])         # receive data from web server
-                if (len(data) > 0):
-                    # print("data : ", data.decode())
-                    conn.send(data)                      # send to browser
-                else:
-                    break
-            s.close()
-            conn.close()
+        try : 
+            if (request.key_word == "GET") :
+                # create a socket to connect to the web server
+                server_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                server_conn.settimeout(config['TIMEOUT'])
+                server_conn.connect((request.host, request.port))
+                server_conn.sendall(r)                           
+
+                while (1) :
+                    data = server_conn.recv(config['MAX_LEN'])         # receive data from web server
+                    if (len(data) > 0):
+                        client_conn.send(data)                      # send to browser
+                    else:
+                        break
+       
+            elif (request.key_word == "CONNECT") : 
+                context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+                context.verify_mode = ssl.CERT_NONE
+                context.check_hostname = False
+
+                server_conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=request.addr)
+                server_conn.settimeout(config['TIMEOUT'])
+                # server_conn.connect((request.host, 443))
+                server_conn.connect((request.host, request.port))
+                # newrequest = "GET "+url+" HTTP/1.1\r\n"+"Host: "+webserver+"\r\n"+"Connection: close\r\n"+"\r\n"
+                # print("request: ", request)
+
+                # request = request.encode()
+                print("request: ", r.decode())
+                server_conn.sendall(r)
+                
+                resp = ""
+                while (1) :
+                    temp = server_conn.recv(config['MAX_LEN']).decode()
+                    if (not temp):
+                        break
+                    resp += temp
+                print("resp: ", resp)
+                client_conn.sendall(resp.encode())
+            
+            server_conn.close()
+            client_conn.close()
+
         except socket.error as error_msg:
             print ("ERROR: ",client_addr,error_msg)
-            if s:
-                s.close()
-            if conn:
-                conn.close()
+            if server_conn:
+                server_conn.close()
+            if client_conn:
+                client_conn.close()
+
 
     # def _parse_request (request) :
     #     first_line = request.split('\n')[0]
