128841662412d91077c48d461ce4de73ca67b544
rgalbiati <raina.galbiati@tufts.edu>
2018-04-26 11:04:35 -0400

    integrated filter changes with https changes

--- proxy.py
+++ proxy.py
@@ -4,6 +4,24 @@ import signal
 import sys
 import contentfilter
 import cache
+from http.server import BaseHTTPRequestHandler
+import io
+
+
+class Request (BaseHTTPRequestHandler):
+    def __init__(self, request) :
+        self.rfile = io.BytesIO(request)
+        self.raw_requestline = self.rfile.readline()
+        self.error_code = self.error_message = None
+        self.parse_request()
+        try:
+            self.host, self.port = self.headers['host'].split(":")
+        except AttributeError :
+            self.host = None
+            self.port = None
+        except :
+            self.port = None
+            self.host = self.headers['host']
 
 class Proxy :
     def __init__(self, config) :
@@ -25,73 +43,30 @@ class Proxy :
         self.__clients = {}
 
     def listen(self) :
-        print("hello")
         while True:
             (cli_sock, cli_addr) = self.serverSocket.accept()
-            print("eyo")
             process = threading.Thread(name=self._get_name(cli_addr), target=self.start_thread, args=(cli_sock, cli_addr))
             process.setDaemon(True)
             process.start()
-            print("hiiii")
         self.shutdown(0,0)
-        print("leaving")
 
-    def start_thread(self, conn, client_addr):
-        """
-        *******************************************
-        *********** PROXY_THREAD FUNC *************
-          A thread to handle request from browser
-        *******************************************
-        """
+    def start_thread(self, client_conn, client_addr):
+        r = client_conn.recv(config['MAX_LEN'])       
 
-        request = conn.recv(config['MAX_LEN'])        # get the request from browser
-        request = request.decode()
-        first_line = request.split('\n')[0]                   # parse the first line
-        print("first line: ", first_line)
-
-        if len(first_line.split(' ')) == 1:
-            # s.close()
-            conn.send("")
-            conn.close()
+        request = Request(r)
+        if (request.host is None) : 
+            client_conn.close()
             return
 
-        url = first_line.split(' ')[1]                        # get url
-        print("url: ", url)
-
-        cacheData = cache.get(url)
+       
+        cacheData = cache.get(request.path)
         if cacheData is not None:
             print("Found in cache!")
-            conn.send(cacheData)
+            client_conn.send(cacheData)
             # s.close()
-            conn.close()
+            client_conn.close()
             return
 
-        # find the webserver and port
-        http_pos = url.find("://")          # find pos of ://
-        if (http_pos==-1):
-            temp = url
-        else:
-            temp = url[(http_pos+3):]       # get the rest of url
-
-        port_pos = temp.find(":")           # find the port pos (if any)
-
-        # find end of web server
-        webserver_pos = temp.find("/")
-        if webserver_pos == -1:
-            webserver_pos = len(temp)
-
-        webserver = ""
-        port = -1
-        if (port_pos==-1 or webserver_pos < port_pos):      # default port
-            port = 80
-            webserver = temp[:webserver_pos]
-        else:                                               # specific port
-            port = int((temp[(port_pos+1):])[:webserver_pos-port_pos-1])
-            webserver = temp[:port_pos]
-
-        print("webserver: ", webserver)
-        print("port: ", port)
-        
         # Content filter - keywords should be automated
         filter = contentfilter.ContentFilter()
         filter.addKeyword("Indigenous")
@@ -99,93 +74,69 @@ class Proxy :
         filter.addKeyword("sexual misconduct")
         filter.addKeyword("assault")
 
-        try:
-            # create a socket to connect to the web server
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.settimeout(config['TIMEOUT'])
-            s.connect((webserver, port))
-            request = request.encode()
-            s.sendall(request)                           # send request to webserver
+        try : 
+            if (request.command == "GET") :
+                if (request.port is None) :
+                    request.port = 80
 
-            while 1:
-                data = s.recv(config['MAX_LEN'])         # receive data from web server
-                if (len(data) > 0):
-                    # print("data : ", data.decode())
-                    try:
-                        data_filtered = filter.filterPage(data.decode())
-                        conn.send(data_filtered.encode())
-                        if data_filtered != "":
-                            cache.add(url, data_filtered.encode())
-                    except UnicodeDecodeError:
-                        conn.send(data)                      # send to browser
-                else:
-                    break
-            s.close()
-            conn.close()
+                server_conn = self.create_sock(request)
+                server_conn.sendall(r)                           
+
+                while 1:
+                    data = server_conn.recv(config['MAX_LEN'])         # receive data from web server
+                    if (len(data) > 0):
+                        # print("data : ", data.decode())
+                        try:
+                            data_filtered = filter.filterPage(data.decode())
+                            client_conn.send(data_filtered.encode())
+                            if data_filtered != "":
+                                cache.add(request.path, data_filtered.encode())
+                        except UnicodeDecodeError:
+                            client_conn.send(data)                      # send to browser
+                    else:
+                        break
+       
+            elif (request.command == "CONNECT") : 
+                
+                if (request.port is None) :
+                    request.port = 443
+                
+                server_conn = self.create_sock(request)
+
+                conn_resp = "HTTP/1.1 200 Connection established\r\n"+"Proxy-agent: Pyx\r\n"+"\r\n"
+                client_conn.sendall(conn_resp.encode())
+
+                print("sent established connection")
+                
+                data = client_conn.recv(config['MAX_LEN'])         
+                server_conn.send(data)
+               
+                print("data sent")
+
+                while (1) :
+                    data = server_conn.recv(config['MAX_LEN'])         
+                    if (len(data) > 0):
+                        client_conn.send(data)
+                    else:
+                        break
+
+            
+            server_conn.close()
+            client_conn.close()
+
         except socket.error as error_msg:
             print ("ERROR: ",client_addr,error_msg)
-            if s:
-                s.close()
-            if conn:
-                conn.close()
+            if server_conn:
+                server_conn.close()
+            if client_conn:
+                client_conn.close()
 
-    # def _parse_request (request) :
-    #     first_line = request.split('\n')[0]
-    #     print("first line: ", first_line)
-    #     url = first_line.split(' ')[1]
-    #     print("url: ", url)
 
-    #     http_pos = url.find("://")
-    #     if (http_pos==-1):
-    #         temp = url
-    #     else:
-    #         temp = url[(http_pos+3):]
-
-    #     port_pos = temp.find(":") 
-
-    #     webserver_pos = temp.find("/")
-    #     if webserver_pos == -1:
-    #         webserver_pos = len(temp)
-
-    #     webserver = ""
-    #     port = -1
-    #     if (port_pos==-1 or webserver_pos < port_pos):      # default port
-    #         port = 80
-    #         webserver = temp[:webserver_pos]
-    #     else:                                               # specific port
-    #         port = int((temp[(port_pos+1):])[:webserver_pos-port_pos-1])
-    #         webserver = temp[:port_pos]
-
-    #     print("webserver: ", webserver)
-    #     print("port: ", port)
-
-    #     return webserver, port, url
-
-    # def start_thread(self, conn, client_addr) : 
-    #     request = conn.recv(config['MAX_LEN'])
-    #     print("request: ", request)
-    #     webserver, port, url = _parse_request(request)
-
-    #     try:
-    #         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #         s.settimeout(config['TIMEOUT'])
-    #         s.connect((webserver, port))
-    #         s.sendall(request)                           
-
-    #         while 1:
-    #             data = s.recv(config['MAX_LEN'])          # receive data from web server
-    #             if (len(data) > 0):
-    #                 conn.send(data)                               # send to browser
-    #             else:
-    #                 break
-    #         s.close()
-    #         conn.close()
-    #     except socket.error as error_msg:
-    #         print ("ERROR: ", client_addr, error_msg)
-    #         if s:
-    #             s.close()
-    #         if conn:
-    #             conn.close()
+    def create_sock(self, request) :
+        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn.settimeout(config['TIMEOUT'])
+        conn.connect((request.host, (int)(request.port)))
+        return conn
 
     def _get_name(self, cli_addr):
         return "Client"
@@ -193,7 +144,6 @@ class Proxy :
     def close_connect(self):
         self.serverSocket.close()
         sys.exit(0)
-
 
 
 if __name__ == "__main__":
@@ -210,7 +160,3 @@ if __name__ == "__main__":
     cache = cache.LFU_Cache(100)
     proxy = Proxy(config)
     proxy.listen()
-
-
-
-
